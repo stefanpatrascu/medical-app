9ec365453a0649e0e0c58222cc0e69b38b7003c0
stefanpatrascu <stefannpatrascu@gmail.com>
2023-04-03 21:04:30 +0300

    Created API for logout

--- backend/src/main/java/com/medical/medicalappointments/controller/AuthController.java
+++ backend/src/main/java/com/medical/medicalappointments/controller/AuthController.java
@@ -1,5 +1,6 @@
 package com.medical.medicalappointments.controller;
 import com.medical.medicalappointments.annotations.RoleRequired;
+import com.medical.medicalappointments.config.JwtConfig;
 import com.medical.medicalappointments.model.dto.LoginRequestDTO;
 import com.medical.medicalappointments.model.enums.Role;
 import com.medical.medicalappointments.service.CustomUserDetailsService;
@@ -13,6 +14,8 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 @RequestMapping("/auth")
@@ -27,14 +30,29 @@ public class AuthController {
     @Autowired
     private JwtService jwtService;
 
+    @Autowired
+    private JwtConfig jwtConfig;
+
     @GetMapping("/test-api")
     @RoleRequired({Role.ADMIN, Role.PATIENT})
     public ResponseEntity<String> test() {
         return new ResponseEntity<>("Test", HttpStatus.OK);
     }
 
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        Cookie logoutCookie = new Cookie("JWT-TOKEN", null);
+        logoutCookie.setHttpOnly(true);
+        logoutCookie.setSecure(true);
+        logoutCookie.setPath("/");
+        logoutCookie.setMaxAge(0); // Set the cookie to expire immediately
+
+        response.addCookie(logoutCookie);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequestDTO loginRequest) {
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequestDTO loginRequest, HttpServletResponse response) {
         Authentication authentication = authenticationManager.authenticate(
             new UsernamePasswordAuthenticationToken(
                 loginRequest.getEmail(),
@@ -43,8 +61,18 @@ public class AuthController {
         );
 
         UserDetails userDetails = customUserDetailsService.loadUserByUsername(loginRequest.getEmail());
-        String jwt = jwtService.generateToken(userDetails.getUsername());
+        String jwtToken = jwtService.generateToken(userDetails.getUsername());
 
-        return ResponseEntity.ok(jwt);
+        // Create HttpOnly cookie
+        Cookie jwtCookie = new Cookie("JWT-TOKEN", jwtToken);
+        jwtCookie.setHttpOnly(true);
+        jwtCookie.setSecure(true);
+        jwtCookie.setPath("/"); // Set the path for the cookie
+        jwtCookie.setMaxAge((int) jwtConfig.getExpirationTime() / 1000); // Set the cookie's max age to the same as the token's
+
+        // Add the cookie to the response
+        response.addCookie(jwtCookie);
+
+        return ResponseEntity.ok("Login successful");
     }
 }
