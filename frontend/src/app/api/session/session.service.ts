daeecd68b0d8b0943f784d5accbce7b0caba5c73
stefanpatrascu <stefannpatrascu@gmail.com>
2023-04-09 03:44:25 +0300

    Removed duplicate Injectable

--- frontend/src/app/api/session/session.service.ts
+++ frontend/src/app/api/session/session.service.ts
@@ -3,9 +3,6 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from "../../../environments/environment";
 import { SessionApiEnum } from "./session.enum";
-@Injectable({
-  providedIn: 'root'
-})
 
 @Injectable({
   providedIn: 'root',
