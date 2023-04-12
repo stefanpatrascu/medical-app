df35b5c5a013f26443cf23f0706b3a6551da381f
stefanpatrascu <stefannpatrascu@gmail.com>
2023-04-12 16:45:08 +0300

    Added JsonInclude

--- backend/src/main/java/com/medical/medicalappointments/model/dto/ResponseEntityDTO.java
+++ backend/src/main/java/com/medical/medicalappointments/model/dto/ResponseEntityDTO.java
@@ -1,5 +1,6 @@
 package com.medical.medicalappointments.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,8 +10,13 @@ import java.util.Date;
 @Getter
 public class ResponseEntityDTO {
     private int status;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String message;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String error;
+
     private Date timestamp;
 
     public ResponseEntityDTO(int status, String message, String error) {
