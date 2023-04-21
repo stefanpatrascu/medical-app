0c854f14ab0199465d8e8dbce3de1ba41f850b72
stefanpatrascu <stefannpatrascu@gmail.com>
2023-04-21 23:26:12 +0300

    refactor

--- backend/src/main/resources/db/migration/V3.2__add_users.sql
+++ backend/src/main/resources/db/migration/V3.2__add_users.sql
@@ -1,46 +1,43 @@
-IF
-NOT EXISTS (SELECT 1 FROM users WHERE email = 'stefanpatrascu96+doctor@gmail.com')
-BEGIN
-INSERT INTO doctor_info (clinic_address)
-    OUTPUT INSERTED.id
-VALUES ('123 Heart Street');
+-- Common variables
+DECLARE @password NVARCHAR(MAX) = '$2a$10$bq2FgYCCnRabeEjLc/GzXuq8FdYYXXate9V/rtecONR5JKNp6ruHW';
 
-DECLARE
-@doctor_info_id BIGINT;
-SET
-@doctor_info_id = SCOPE_IDENTITY();
-INSERT INTO users (first_name, last_name, password, email, role, avatar_file_name, doctor_info_id, patient_info_id)
-VALUES ('Stefan', 'Doctor', '$2a$10$bq2FgYCCnRabeEjLc/GzXuq8FdYYXXate9V/rtecONR5JKNp6ruHW', 'stefanpatrascu96+doctor@gmail.com', 'DOCTOR', null, @doctor_info_id, null);
+-- Insert doctor if not already in the users table
+IF NOT EXISTS (SELECT 1 FROM users WHERE email = 'stefanpatrascu96+doctor@gmail.com')
+    BEGIN
+        INSERT INTO doctor_info (clinic_address)
+        OUTPUT INSERTED.id
+        VALUES ('123 Heart Street');
 
-INSERT INTO doctor_specialization (doctor_info_id, specialization_id)
-VALUES (@doctor_info_id, (SELECT TOP 1 MIN(id) FROM medical_specializations ));
+        DECLARE @doctor_info_id BIGINT;
+        SET @doctor_info_id = SCOPE_IDENTITY();
 
-INSERT INTO doctor_specialization (doctor_info_id, specialization_id)
-VALUES (@doctor_info_id, (SELECT TOP 1 MAX(id) FROM medical_specializations ));
+        INSERT INTO users (first_name, last_name, password, email, role, avatar_file_name, doctor_info_id, patient_info_id)
+        VALUES ('Stefan', 'Doctor', @password, 'stefanpatrascu96+doctor@gmail.com', 'DOCTOR', null, @doctor_info_id, null);
 
-END
+        INSERT INTO doctor_specialization (doctor_info_id, specialization_id)
+        VALUES (@doctor_info_id, (SELECT TOP 1 MIN(id) FROM medical_specializations ));
 
-IF
-NOT EXISTS (SELECT 1 FROM users WHERE email = 'stefanpatrascu96+patient@gmail.com')
-BEGIN
+        INSERT INTO doctor_specialization (doctor_info_id, specialization_id)
+        VALUES (@doctor_info_id, (SELECT TOP 1 MAX(id) FROM medical_specializations ));
+    END
 
-INSERT INTO patient_info (address, phone_number)
-    OUTPUT INSERTED.id
-VALUES ('456 Wellness Avenue', '555-123-4567');
+-- Insert patient if not already in the users table
+IF NOT EXISTS (SELECT 1 FROM users WHERE email = 'stefanpatrascu96+patient@gmail.com')
+    BEGIN
+        INSERT INTO patient_info (address, phone_number)
+        OUTPUT INSERTED.id
+        VALUES ('456 Wellness Avenue', '555-123-4567');
 
-DECLARE
-@patient_info_id BIGINT;
-SET
-@patient_info_id = SCOPE_IDENTITY();
-INSERT INTO users (first_name, last_name, password, email, role, avatar_file_name, doctor_info_id, patient_info_id)
-VALUES ('Stefan', 'Patient', '$2a$10$bq2FgYCCnRabeEjLc/GzXuq8FdYYXXate9V/rtecONR5JKNp6ruHW', 'stefanpatrascu96+patient@gmail.com', 'PATIENT', null, null,
-        @patient_info_id);
-END
+        DECLARE @patient_info_id BIGINT;
+        SET @patient_info_id = SCOPE_IDENTITY();
 
-IF
-NOT EXISTS (SELECT 1 FROM users WHERE email = 'stefanpatrascu96+admin@gmail.com')
-BEGIN
+        INSERT INTO users (first_name, last_name, password, email, role, avatar_file_name, doctor_info_id, patient_info_id)
+        VALUES ('Stefan', 'Patient', @password, 'stefanpatrascu96+patient@gmail.com', 'PATIENT', null, null, @patient_info_id);
+    END
 
-INSERT INTO users (first_name, last_name, password, email, role, avatar_file_name, doctor_info_id, patient_info_id)
-VALUES ('Stefan', 'Admin', '$2a$10$bq2FgYCCnRabeEjLc/GzXuq8FdYYXXate9V/rtecONR5JKNp6ruHW', 'stefanpatrascu96+admin@gmail.com', 'ADMIN', null, null, null);
-END
+-- Insert admin if not already in the users table
+IF NOT EXISTS (SELECT 1 FROM users WHERE email = 'stefanpatrascu96+admin@gmail.com')
+    BEGIN
+        INSERT INTO users (first_name, last_name, password, email, role, avatar_file_name, doctor_info_id, patient_info_id)
+        VALUES ('Stefan', 'Admin', @password, 'stefanpatrascu96+admin@gmail.com', 'ADMIN', null, null, null);
+    END
